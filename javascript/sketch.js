f32bb9b1520e9c50f0cb0fa53ee3d90e53679e93
ievavaiti <146743152+ieva-gsmd@users.noreply.github.com>
2023-10-22 22:25:32 +0100

    comments

--- javascript/sketch.js
+++ javascript/sketch.js
@@ -1,45 +1,54 @@
 
 function setup() {
   createCanvas(600, 600);
+
+  //make background black
   background(0);
 }
 
 function draw() {
 
-  if (mouseIsPressed) {
+  if (mouseIsPressed) { //if moused is pressed
+    // execute this function at 60 frames per second
      mySquares(mouseX, mouseY, 20, 20);
-    // translate(mouseX, mouseY);
+
+     //uncomment this code for the pattern function
+    // translate(mouseX, mouseY); //use translate to move position instead
     // squareInside(0, 0);
   }
-
   
 }
 
 
+//functions:
+
+//square drawing function
 function mySquares(x, y) {
 
-  noFill();
-  stroke(255);
-  rectMode(CENTER);
+  noFill(); //set the squares to have no fill
+  stroke(255); //stroke to be white
+  rectMode(CENTER); //make the center of the square the x,y value
   
-
+  //map the size of the square to the width of the mouse x position. Use negative values so the square is smallest in the middle
   w_ = map(mouseX, 0, width, -100, 100);
 
-
+  //draw square
   rect(x, y, w_, w_);
 
 }
 
+//pattern
 function squareInside(x, y) {
   
+  //again, set the drawing functions
   noFill();
   stroke(255);
   rectMode(CENTER);
 
-  for (let i = 80; i >=20; i -= 10) {
+  //create a square within a square pattern
+  for (let i = 80; i >=20; i -= 10) { //start with 80, and reduce width by 10 pixels until it reaches 20 pixels
     rect(x, y, i, i);
     
   }
-
 
 }
